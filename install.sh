3b1ce8ecbae1dbe39d3cd3bcbd104a11d6b3a3d4
dstoffels <dan.stoffels@gmail.com>
2023-09-22 19:46:57 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
-sudo mkdir -p /var/dcc-scripts
-sudo cp -r . /var/dcc-scripts
-sh ./scripts/system.sh
-sh ./scripts/db.sh
-sh ./scripts/package.sh
-sh ./scripts/clone.sh
-cd ./scripts//var/canvas
-sh ./scripts/config.sh
-sh ./scripts/deps.sh
-sh ./scripts/apache.sh
-# sh ./scripts/ssl.sh #production
-sh ./scripts/ssl-test.sh # test
-sh ./scripts/vhosts.sh
-sh ./scripts/jobs.sh
-sh ./scripts/optim.sh
-sh ./scripts/rce.sh
+mkdir -p /var/dcc-scripts
+cp . /var/dcc-scripts
+sh /var/dcc-scripts/scripts/system.sh
+sh /var/dcc-scripts/scripts/db.sh
+sh /var/dcc-scripts/scripts/package.sh
+sh /var/dcc-scripts/scripts/clone.sh
+cd /var/canvas
+sh /var/dcc-scripts/scripts/config.sh
+sh /var/dcc-scripts/scripts/deps.sh
+sh /var/dcc-scripts/scripts/apache.sh
+# s/var/dcc-scripts ./scripts/ssl.sh #production
+sh /var/dcc-scripts/scripts/ssl-test.sh # test
+sh /var/dcc-scripts/scripts/vhosts.sh
+sh /var/dcc-scripts/scripts/jobs.sh
+sh /var/dcc-scripts/scripts/optim.sh
+sh /var/dcc-scripts/scripts/rce.sh
 
 
 
