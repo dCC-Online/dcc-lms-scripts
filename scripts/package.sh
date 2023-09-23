45278473600645554ec667b85c73ee9cfbff15a2
dstoffels <dan.stoffels@gmail.com>
2023-09-23 00:03:20 -0500

    fix: test node version: package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y software-properties-common;
 sudo add-apt-repository -y ppa:instructure/ruby; 
 sudo apt-get update; 
 sudo apt-get install -y ruby3.1 ruby3.1-dev zlib1g-dev libxml2-dev libsqlite3-dev postgresql libpq-dev libxmlsec1-dev libidn11-dev curl make g++; 
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -; # will display a version error, can ignore.
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -; # will display a version error, can ignore.
 sudo apt-get install -y nodejs; 
 sudo npm install -g npm@latest; 
 curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.19.1;
