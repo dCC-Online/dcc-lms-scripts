58dd0308271fb3b6fd1a7adfb167a4688c3e6bc7
dstoffels <dan.stoffels@gmail.com>
2023-09-22 23:59:22 -0500

    fix: package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,7 +3,7 @@
 # 3. Installing Git, Ruby, Node.js, and Yarn
 sudo apt-get install -y git-core; 
 sudo apt-get install -y software-properties-common; 
-sudo add-apt-repository ppa:instructure/ruby; 
+sudo add-apt-repository -y ppa:instructure/ruby; 
 sudo apt-get update; 
 sudo apt-get install -y ruby3.1 ruby3.1-dev zlib1g-dev libxml2-dev libsqlite3-dev postgresql libpq-dev libxmlsec1-dev libidn11-dev curl make g++; 
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -; # will display a version error, can ignore.
