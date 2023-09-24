96f6ca7bc4664e70bad65aac8c7c9c5fe974d718
dstoffels <dan.stoffels@gmail.com>
2023-09-24 04:14:05 -0500

    fix: update db.sh

--- scripts/db.sh
+++ scripts/db.sh
@@ -3,7 +3,6 @@
 sudo apt-get install -y postgresql-12; 
 sudo -u postgres createuser canvas --no-createdb --no-superuser --no-createrole;
 sudo -u postgres psql -c "ALTER USER canvas WITH PASSWORD '$DB_PASSWORD';"
-sudo -u postgres createdb canvas_production --owner=canvas; 
-sudo -u postgres createdb canvas_development --owner=canvas; 
+sudo -u postgres createdb $DB_NAME --owner=canvas; 
 sudo -u postgres createuser $USER; 
 sudo -u postgres psql -c "alter user $USER with superuser" postgres;
