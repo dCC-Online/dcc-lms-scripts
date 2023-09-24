dc329a43a5f374543e2d2b03915b5f84df851172
dstoffels <dan.stoffels@gmail.com>
2023-09-24 14:52:55 -0500

    fix: update SSL whitelist

--- scripts/ssl-staging.sh
+++ scripts/ssl-staging.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-
-# 8. Obtain SSL Certificate For Your Domain
-# sudo apt update; 
-# sudo apt install -y certbot;
-# sudo apt install -y python3-certbot-apache;
-
 # # TEST: LOCALHOST SSL
 cd /var/canvas
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $SSL_CERT_KEY_PATH -out $SSL_CERT_PATH -config /var/dcc-scripts/config/openssl.conf
 
+# add self-signed SSL certs to VM's whitelist
+sudo cp /var/canvas/fullchain.pem /usr/local/share/ca-certificates/fullchain.crt
+sudo cp /var/canvas/privkey.pem /usr/local/share/ca-certificates/privkey.crt
 
-# sudo apt install libnss3-tools
-# wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64
-# sudo mv mkcert-v1.4.3-linux-amd64 /usr/local/bin/mkcert
-# sudo chmod +x /usr/local/bin/mkcert
-# mkcert -install
-# mkcert $DOMAIN $RCE_SUBDOMAIN
+# must add self-signed SSL to node's whitelist
+export NODE_EXTRA_CA_CERTS=/var/canvas/fullchain.pem  
