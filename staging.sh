6b1256ed79b2ef2adccbca63788ed65deb691f95
dstoffels <dan.stoffels@gmail.com>
2023-09-24 02:53:03 -0500

    fix: staging.sh

--- staging.sh
+++ staging.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-if [[ ! -f .env.staging ]]; then
+if [[ -f .env.staging ]]; then
+  source ./.env.staging
+
+  . scripts/system.sh
+  . scripts/db.sh
+  . scripts/package.sh
+  . scripts/clone.sh
+  . scripts/config.sh
+  . scripts/deps.sh
+  . scripts/apache.sh
+  . scripts/ssl-staging.sh
+  . scripts/vhosts.sh
+  . scripts/jobs.sh
+  . scripts/optim.sh
+  . scripts/rce.sh
+else
   echo "This script requires a .env.staging file to run. You can acquire one from the danimal or create your own from the README instructions."
 fi
 
-source ./.env.staging
-
-. scripts/system.sh
-. scripts/db.sh
-. scripts/package.sh
-. scripts/clone.sh
-. scripts/config.sh
-. scripts/deps.sh
-. scripts/apache.sh
-. scripts/ssl-staging.sh
-. scripts/vhosts.sh
-. scripts/jobs.sh
-. scripts/optim.sh
-. scripts/rce.sh
