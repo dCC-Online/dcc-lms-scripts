15fbba22a840f54c0f476df560b0da93946777f4
dstoffels <dan.stoffels@gmail.com>
2023-09-24 11:19:49 -0500

    fix: update ssl > openssl

--- scripts/ssl-staging.sh
+++ scripts/ssl-staging.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-cd /var/dcc-scripts/
-sudo cp config/options-ssl-apache.conf /etc/letsencrypt/options-ssl-apache.conf
 
 # 8. Obtain SSL Certificate For Your Domain
-cd /var/canvas
-sudo apt update; 
-sudo apt install -y certbot;
-sudo apt install -y python3-certbot-apache;
+# sudo apt update; 
+# sudo apt install -y certbot;
+# sudo apt install -y python3-certbot-apache;
 
-# TEST: LOCALHOST SSL
-sudo apt update
-sudo apt install libnss3-tools
-wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64
-sudo mv mkcert-v1.4.3-linux-amd64 /usr/local/bin/mkcert
-sudo chmod +x /usr/local/bin/mkcert
-mkcert -install
-mkcert $DOMAIN $RCE_SUBDOMAIN
+# # TEST: LOCALHOST SSL
+cd /var/canvas
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $SSL_CERT_KEY_PATH -out $SSL_CERT_PATH -config /var/dcc-scripts/config/openssl.conf
+
+
+# sudo apt install libnss3-tools
+# wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64
+# sudo mv mkcert-v1.4.3-linux-amd64 /usr/local/bin/mkcert
+# sudo chmod +x /usr/local/bin/mkcert
+# mkcert -install
+# mkcert $DOMAIN $RCE_SUBDOMAIN
--- scripts/vhosts.sh
+++ scripts/vhosts.sh
@@ -11,6 +11,7 @@ sudo cp config/canvas.conf /etc/apache2/sites-available/canvas.conf
 sudo sed -i "s|DOMAIN|$DOMAIN|g" config/canvas-ssl.conf
 sudo sed -i "s|SSL_CERT_PATH|$SSL_CERT_PATH|g" config/canvas-ssl.conf
 sudo sed -i "s|SSL_CERT_KEY_PATH|$SSL_CERT_KEY_PATH|g" config/canvas-ssl.conf
+sudo sed -i "s|SSL_OPTIONS_CONF|$SSL_OPTIONS_CONF|g" config/canvas-ssl.conf
 sudo cp config/canvas-ssl.conf /etc/apache2/sites-available/canvas-ssl.conf
 
 
