b16155ff5bff708f8ef77f0fb16fe48ebf3a8b6f
dstoffels <dan.stoffels@gmail.com>
2023-09-24 05:05:39 -0500

    fix: pathing

--- scripts/vhosts.sh
+++ scripts/vhosts.sh
@@ -6,14 +6,14 @@ sudo unlink /etc/apache2/sites-enabled/000-default.conf
 
 cd /var/dcc-scripts
 sudo sed -i "s|DOMAIN|$DOMAIN|g" config/canvas.conf
-sudo cp config/canvas.conf /var/canvas/config/canvas.conf
+sudo cp config/canvas.conf /etc/apache2/sites-available/canvas.conf
 
 sudo sed -i "s|DOMAIN|$DOMAIN|g" config/canvas-ssl.conf
 sudo sed -i "s|SSL_CERT_PATH|$SSL_CERT_PATH|g" config/canvas-ssl.conf
 sudo sed -i "s|SSL_CERT_KEY_PATH|$SSL_CERT_KEY_PATH|g" config/canvas-ssl.conf
-sudo cp config/canvas-ssl.conf /var/canvas/config/canvas-ssl.conf
+sudo cp config/canvas-ssl.conf /etc/apache2/sites-available/canvas-ssl.conf
 
 
 cd /var/canvas
-sudo a2ensite /var/canvas/config/canvas.conf; 
-sudo a2ensite /var/canvas/config/canvas-ssl.conf;
+sudo a2ensite canvas.conf; 
+sudo a2ensite canvas-ssl.conf;
